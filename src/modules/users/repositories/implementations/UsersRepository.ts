6e18bd6220873c9b3b9c30c47c2cd305179909fc
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-06-08 19:23:41 -0300

    Refactoring the UsersRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -34,13 +34,11 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    const user = this.users.find((user) => user.id === id);
-    return user;
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    const user = this.users.find((user) => user.email === email);
-    return user;
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
